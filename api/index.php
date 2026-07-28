7ff0ac4c16d9f21ad548ad1aecaab00e1f0561a4
mindiq <hfdhirfan@gmail.com>
2026-07-28 11:17:05 +0700

    Fix Vercel PHP configuration using builds array to prevent downloading index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
-// Forward requests to public/index.php for Laravel
+// Forward requests to public/index.php for Laravel on Vercel
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+
 require __DIR__ . '/../public/index.php';
